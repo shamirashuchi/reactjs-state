08e42c851737c6ec8eb299d57471b07f26305b81
shamirashuchi <shamirashuchi@gmail.com>
2022-10-06 16:48:49 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Counter></Counter>
+    </div>
+  );
+}
+
+function Counter(){
+  const [count,setCount] = useState(55);
+
+  const increaseCount = () =>setCount(count+1);
+  const decreaseCount = () =>setCount(count-1);
+  // const increaseCount = () =>{
+  //   const newCount = count+1;
+  //   setCount(newCount);
+  // }
+  //console.log(abc);
+  return (
+    <div>
+      <h1>Count{count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  )
+}
+
+export default App;
